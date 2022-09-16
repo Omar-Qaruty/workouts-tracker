c0efbe8afd2fd8f569f08682d452f6a061705287
knightSarai <knight.sarai.dev@gmail.com>
2022-09-16 17:31:33 +0300

    Add nodemon and http main

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send({
+    message: ' Hello World!'
+  })
 })
 
 app.listen(port, () => {
